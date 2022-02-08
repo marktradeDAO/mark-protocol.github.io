a2f28c41b75b6bcc0c6a03e0cef42d2eb2b6d6e0
Erik Young <erik_young@mac.com>
2022-02-08 17:20:00 -0500

    Added new logo, explanatory text and exchange illustration.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
 import './App.css';
-import  mtInitials from './assets/svg/mt-initials.svg'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import creatorBrandExchange from './marktrade_creator_brand_exchange.svg'
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import { makeStyles } from "@mui/styles";
+import MarkTradeWebTheme from './MarkTradeWebTheme'
+import markTradeLogo from './marktrade_logo_2.svg'
+import { ThemeProvider } from '@mui/material/styles'
+import Toolbar from '@mui/material/Toolbar';
 
 function App() {
+  const explainerTextStyle = {
+    color: "#000000"
+  }
+
+  const explainerTextHighlightStyle = {
+    color: "#000080"
+  }
+
+  const logoStyle = {
+    width: "10%",
+    height: "10%"
+  }
+
+  const creatorBrandExchangeStyle = {
+    width: "70%",
+    height: "70%"
+  }
+
+  const gridStyle = {
+    margin: "72px"
+  }
+
+  const gridItemStyle = {
+    textAlign: 'left'
+  }
+
   return (
+    <ThemeProvider theme={MarkTradeWebTheme}>
     <div className="App">
-      <div className="landing-container">
-        <div className="landing-container__background-gradient"></div>
-        <p className="landing-container__initials-mt">MT</p>
-        <div><img src={mtInitials} className="landing-container__initials-mt-svg" alt="logo" /></div>
-        <div className="landing-container__infos">
-          <p className="landing-container__title">Your Identity. Your Brand.</p>
-          <p className="landing-container__title">Your <span className="landing-container__info-highlight">Mark</span>.</p>
-          <p className="landing-container__subtitle">The Mark Token is coming.</p>
-        </div>
-      </div>
+  
+      <Grid container spacing={2} sx={gridStyle}>
+        
+        <Grid item xs={12}>
+        <AppBar className={AppBar} position="static">
+              <Toolbar disableGutters>
+                <img className="markTradeLogo" style={logoStyle} src={markTradeLogo} alt="This is the MarkTrade logo image"/>
+              </Toolbar>
+          </AppBar>
+        </Grid>
+
+        <Grid item style={gridItemStyle} xs={4}>
+          <h1 style={explainerTextStyle}> <span style={explainerTextHighlightStyle}>MarkTrade</span> is a community and marketplace where creators and brand owners meet to exchange unique proofs of identity and branding called “Marks”.</h1>
+        </Grid>
+        <Grid item xs={8}>
+          <img className="creatorBrandExchange" style={creatorBrandExchangeStyle} src={creatorBrandExchange} alt="Creators and brands can exchange Marks"/>
+        </Grid>
+        <Grid item xs={4}>
+
+        </Grid>
+
+      </Grid>
+
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/MarkTradeWebTheme.jsx
@@ -0,0 +1,110 @@
+import { createTheme } from "@mui/material/styles";
+
+// Create a MUI theme here.
+const MarkTradeWebTheme = createTheme({
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#ffffff',
+      },
+      secondary: {
+        main: '#000080',
+        contrastText: '#FFFFFF',
+        light: '#173a5e',
+        dark: '#173a5e',
+      },
+      divider: '#265d97',
+      background: {
+        default: '#ffffff',
+        paper: '#ffffff',
+      },
+      text: {
+        primary: 'navy',
+        secondary: 'navy',
+        disabled: '#344c64',
+        hint: '#199c50',
+      }
+    }, 
+    components: {
+      MarkTradeLogo: {
+        width: '50%',
+        height: '50%'
+      },
+      Container: {
+        styleOverrides: {
+          root: {
+            justifyContent: 'center'
+          }
+        }
+      },
+      AppBar: {
+        styleOverrides: {
+          root: {
+            backgroundColor: '#ffffff',
+            boxShadow: 'none'
+          }
+        }
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            '&:hover': {
+              backgroundColor: '#132f4c',
+              boxShadow: 'none'
+            },
+            textTransform: 'none',
+            boxShadow: 'none'
+          }
+        },
+        defaultProps: {
+          disableRipple: true,
+        },
+      },
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+            backgroundColor: '#ffffff',
+            boxShadow: 'none'
+          }
+        }
+      },
+      MuiToolbar: {
+        styleOverrides: {
+          root: {
+            boxShadow: 'none'
+          }
+        }
+      },
+      MuiTypography: {
+        defaultProps: {
+          variantMapping: {
+            h1: 'h2',
+            h2: 'h2',
+            h3: 'h2',
+            h4: 'h2',
+            h5: 'h2',
+            h6: 'h2',
+            subtitle1: 'h2',
+            subtitle2: 'h2',
+            body1: 'span',
+            body2: 'span',
+          },
+        },
+      },
+    },
+    typography: {
+      h1: {
+        color: '#000080'
+      },
+      fontFamily: [
+        'Roboto'
+      ]
+    }
+  });
+
+  MarkTradeWebTheme.typography.h1 = {
+      fontSize: '2.2rem'
+  }
+
+
+export default MarkTradeWebTheme;
